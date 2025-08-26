bbb09f70e075dd970e8d9666d77ec049822ea670
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-08-26 01:52:09 -0700

    update again

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
 
-app.use(express.static(path.join(__dirname, "client", "dist")));
+// app.use(express.static(path.join(__dirname, "client", "dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 app.use((err, req, res, next) => {
   const statusCode = Number(err.statusCode) || 500;
@@ -54,6 +54,7 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 
 const PORT = process.env.PORT || 8000;
