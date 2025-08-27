8e2d2ba05d12935a88e71ac5c93298368e60b961
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-08-27 08:26:40 -0700

    fix index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose
 
 
 const allowedOrigins = [
-  "https://mern-estate-backend-ordh-5ayod6d3f-abdul-kreem-shahs-projects.vercel.app/", 
+  "https://mern-estate-backend-ordh.vercel.app/", 
   "http://localhost:5173" 
 ];
 app.use(cors({
