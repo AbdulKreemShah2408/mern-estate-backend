cd8d55ee30353f2dcef521558327a94de3c26c8f
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-09-01 21:17:40 -0700

    some login fix error

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,18 +21,15 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found"));
-
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
-
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
     });
-
     const { password: pass, ...rest } = validUser._doc;
-
     res.status(200).json({
       user: rest,
-      token, 
+      token,
     });
   } catch (error) {
     next(error);
@@ -42,12 +39,11 @@ export const signin = async (req, res, next) => {
 export const google = async (req, res, next) => {
   try {
     let user = await User.findOne({ email: req.body.email });
-
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
       });
       const { password: pass, ...rest } = user._doc;
-
       return res.status(200).json({
         user: rest,
         token,
@@ -57,7 +53,6 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashPassword = bcrypt.hashSync(generatedPassword, 10);
-
       const newUser = new User({
         username:
           req.body.name.split(" ").join("").toLowerCase() +
@@ -66,13 +61,11 @@ export const google = async (req, res, next) => {
         password: hashPassword,
         avatar: req.body.photo,
       });
-
       await newUser.save();
-
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
       });
       const { password: pass, ...rest } = newUser._doc;
-
       return res.status(200).json({
         user: rest,
         token,
